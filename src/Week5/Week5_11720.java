29e93eae8dc0a400dfcb1150821808826d088987
Xeohn <165402279+xeohyun@users.noreply.github.com>
2024-10-08 18:39:12 +0900

    큐

--- src/Week5/Week5_11720.java
+++ src/Week5/Week5_11720.java
@@ -1,2 +1,21 @@
-package Week5;public class Week5_11720 {
+package Week5;
+
+import java.util.Scanner;
+
+public class Week5_11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String str = sc.next();
+        int sum = 0;
+
+        sc.close();
+
+        for(int i=0;i<n;i++) {
+            sum += str.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+    }
 }
