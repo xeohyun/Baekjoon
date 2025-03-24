ba57d97d29f37c70b327d006eb121581ed97b5bb
Park Seohyun <165402279+xeohyun@users.noreply.github.com>
2025-03-24 16:53:48 +0900

    Day1

---
+++ src/Algorithm/Q1330.java
@@ -0,0 +1,14 @@
+package Algorithm;
+import java.util.Scanner;
+
+public class Q1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if(A>B) System.out.println(">");
+        else if(A<B) System.out.println("<");
+        else System.out.println("==");
+    }
+}
