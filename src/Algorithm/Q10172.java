ed1ed2b006a0bba5706fcd9e26b9ca5dff774724
Park Seohyun <165402279+xeohyun@users.noreply.github.com>
2025-03-29 18:14:18 +0900

    Day6

---
+++ src/Algorithm/Q10172.java
@@ -0,0 +1,11 @@
+package Algorithm;
+
+public class Q10172 {
+    public static void main(String[] args){
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n"+
+                "( 0 )\"\"\"\\\n"+
+                "|\"^\"`    |\n"+
+                "||_/=\\\\__|");
+    }
+}
---
+++ src/Algorithm/Q18108.java
@@ -0,0 +1,12 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class Q18108 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year - 543);
+    }
+}
